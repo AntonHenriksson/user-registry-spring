b07bc76a37c490ceea33b7977b758f9b28195337
Anton Henriksson <henriksson.anton@gmail.com>
2026-04-08 16:39:13 +0200

    first commit setting the repo up

---
+++ src/main/java/se/anton/userregistryspring/UserRegistrySpringApplication.java
@@ -0,0 +1,13 @@
+package se.anton.userregistryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrySpringApplication.class, args);
+    }
+
+}
